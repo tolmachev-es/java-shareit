270e9cb24b6050cbd664633a785714bce94d4feb
tolmachev-es <tolmachev.e.s@yandex.ru>
2023-12-18 08:29:46 +0300

    Описан объект класса Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private boolean isAvailable;
+    private User owner;
+    private ItemRequest itemRequest;
 }
