fd406bf804923832653541f3fe769e02c119d158
tolmachev-es <tolmachev.e.s@yandex.ru>
2023-12-18 08:30:35 +0300

    Описаны Dto и мапперы

---
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.item;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.isAvailable());
+    }
+}
