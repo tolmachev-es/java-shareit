bbe05c4e7f4737f1e9cb256c2d732ffda1051e7c
tolmachev-es <tolmachev.e.s@yandex.ru>
2023-12-19 18:20:59 +0300

    Добавлены параметры валидации для Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -4,14 +4,22 @@ import lombok.Data;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 /**
  * TODO Sprint add-controllers.
  */
 @Data
 public class Item {
     private Long id;
+    @NotBlank
+    @NotEmpty
     private String name;
+    @NotBlank
+    @NotEmpty
     private String description;
+    @NotEmpty
     private boolean isAvailable;
     private User owner;
     private ItemRequest itemRequest;
