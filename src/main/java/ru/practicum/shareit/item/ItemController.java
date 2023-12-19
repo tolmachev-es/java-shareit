dc95f3c49ca1f053b684ec09c8e44531fdd550cf
tolmachev-es <tolmachev.e.s@yandex.ru>
2023-12-19 18:22:18 +0300

    Добавлена эндпоинт метода create для Item

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,25 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.service.ItemService;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
 @RequestMapping("/items")
+@RequiredArgsConstructor
 public class ItemController {
+    private final ItemService itemService;
+
+    @PostMapping
+    public ItemDto create(@RequestHeader("X-Sharer-User-Id") long userId,
+                          @RequestBody Item item) {
+        return itemService.create(item, userId);
+    }
+
+
 }
