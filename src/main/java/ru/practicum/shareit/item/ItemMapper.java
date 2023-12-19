9df66b7bbf767bb58476d8460acc17ab923b1fca
tolmachev-es <tolmachev.e.s@yandex.ru>
2023-12-19 23:00:12 +0300

    изменен маппинг для вещей

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,13 +1,12 @@
 package ru.practicum.shareit.item;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
-public class ItemMapper {
-    public static ItemDto toItemDto(Item item) {
-        return new ItemDto(item.getId(),
-                item.getName(),
-                item.getDescription(),
-                item.isAvailable());
-    }
+@Mapper
+public interface ItemMapper {
+    ItemMapper ITEM_MAPPER = Mappers.getMapper(ItemMapper.class);
+    ItemDto toDto(Item item);
 }
