18456b26dd364d7321fc4ba0c1e8fbf0c60bef36
tolmachev-es <tolmachev.e.s@yandex.ru>
2023-12-21 11:06:09 +0300

    Убраны аннотации из User как неиспользуемые. Добавлены аннотации для UserDto в связи с изменениями в контроллере

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,12 +1,21 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.practicum.shareit.user.model.validationGroups.UserOnCreate;
+import ru.practicum.shareit.user.model.validationGroups.UserOnUpdate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Data
-@AllArgsConstructor
 public class UserDto {
     private Long id;
+    @NotEmpty(groups = UserOnCreate.class)
+    @NotBlank(groups = UserOnCreate.class)
     private String name;
+    @NotEmpty(groups = UserOnCreate.class)
+    @NotBlank(groups = UserOnCreate.class)
+    @Email(groups = {UserOnCreate.class, UserOnUpdate.class})
     private String email;
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,12 +1,6 @@
 package ru.practicum.shareit.user.model;
 
 import lombok.Data;
-import ru.practicum.shareit.user.model.validationGroups.UserOnCreate;
-import ru.practicum.shareit.user.model.validationGroups.UserOnUpdate;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 /**
  * TODO Sprint add-controllers.
@@ -14,11 +8,6 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class User {
     private Long id;
-    @NotEmpty(groups = UserOnCreate.class)
-    @NotBlank(groups = UserOnCreate.class)
     private String name;
-    @NotEmpty(groups = UserOnCreate.class)
-    @NotBlank(groups = UserOnCreate.class)
-    @Email(groups = {UserOnCreate.class, UserOnUpdate.class})
     private String email;
 }
