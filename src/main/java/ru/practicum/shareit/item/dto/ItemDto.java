c272f110d0b74ecf4411af34cda431b7195f4371
tolmachev-es <tolmachev.e.s@yandex.ru>
2023-12-21 13:21:57 +0300

    Изменены правила валидации для объектов класса

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,6 +1,11 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.Data;
+import ru.practicum.shareit.item.model.validationGroups.ItemOnCreate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 /**
  * TODO Sprint add-controllers.
@@ -8,7 +13,12 @@ import lombok.Data;
 @Data
 public class ItemDto {
     private Long id;
+    @NotBlank(groups = ItemOnCreate.class)
+    @NotEmpty(groups = ItemOnCreate.class)
     private String name;
+    @NotBlank(groups = ItemOnCreate.class)
+    @NotEmpty(groups = ItemOnCreate.class)
     private String description;
+    @NotNull(groups = ItemOnCreate.class)
     private Boolean available;
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,29 +1,17 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.Data;
-import lombok.ToString;
-import ru.practicum.shareit.item.model.validationGroups.ItemOnCreate;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.model.User;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 /**
  * TODO Sprint add-controllers.
  */
 @Data
-@ToString
 public class Item {
     private Long id;
-    @NotBlank(groups = ItemOnCreate.class)
-    @NotEmpty(groups = ItemOnCreate.class)
     private String name;
-    @NotBlank(groups = ItemOnCreate.class)
-    @NotEmpty(groups = ItemOnCreate.class)
     private String description;
-    @NotNull(groups = ItemOnCreate.class)
     private Boolean available;
     private User owner;
     private ItemRequest itemRequest;
