2c0ee1d5d60fa39d5a83756fc96d331c95381008
tolmachev-es <tolmachev.e.s@yandex.ru>
2024-01-18 22:33:37 +0300

    Добавлено логирование

--- src/main/java/ru/practicum/shareit/booking/controllers/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controllers/BookingController.java
@@ -1,6 +1,10 @@
 package ru.practicum.shareit.booking.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDtoRequest;
@@ -17,37 +21,69 @@ import java.util.Set;
 @RestController
 @RequestMapping(path = "/bookings")
 @AllArgsConstructor
+@Slf4j
 public class BookingController {
     public final BookingService bookingService;
 
     @PostMapping
+    @Operation(summary = "Создание бронирования")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Booking create"),
+            @ApiResponse(responseCode = "404", description = "Not found"),
+            @ApiResponse(responseCode = "400", description = "Unsupported method")
+    })
     public BookingDtoResponse create(@NotNull @RequestHeader("X-Sharer-User-Id") long userId,
                                      @Validated(BookingOnCreate.class) @RequestBody BookingDtoRequest bookingRequest) {
+        log.info("Получен запрос на создание бронирования");
         return bookingService.create(bookingRequest, userId);
     }
 
     @PatchMapping("/{bookingId}")
+    @Operation(summary = "Подтверждение бронирования")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Booking approved"),
+            @ApiResponse(responseCode = "404", description = "Approved unsupported")
+    })
     public BookingDtoResponse approve(@NotNull @PathVariable long bookingId,
                                       @NotNull @RequestHeader("X-Sharer-User-Id") long userId,
                                       @RequestParam boolean approved) {
+        log.info("Получен запрос на подтверждение бронирования");
         return bookingService.approve(bookingId, userId, approved);
     }
 
     @GetMapping("/{bookingId}")
+    @Operation(summary = "Получение бронирования")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Booking get"),
+            @ApiResponse(responseCode = "404", description = "Booking not found")
+    })
     public BookingDtoResponse get(@NotNull @PathVariable long bookingId,
                                   @NotNull @RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("Получен запрос на получение бронирования с id {}", bookingId);
         return bookingService.get(bookingId, userId);
     }
 
     @GetMapping
+    @Operation(summary = "Получение бронирований по букеру")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful get"),
+            @ApiResponse(responseCode = "404", description = "Unsupported method")
+    })
     public Set<BookingDtoResponse> getByBooker(@RequestParam(required = false, defaultValue = "ALL") String state,
                                                @NotNull @RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("Получен запрос на получение всех бронирований пользователя {}", userId);
         return bookingService.getByBooker(state, userId);
     }
 
     @GetMapping("/owner")
+    @Operation(summary = "Получение бронирований по владельцу")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful get"),
+            @ApiResponse(responseCode = "404", description = "Unsupported method")
+    })
     public Set<BookingDtoResponse> getByOwner(@RequestParam(required = false, defaultValue = "ALL") String state,
                                               @NotNull @RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("Получен запрос на получение бронирований по владельцу с id {}", userId);
         return bookingService.getByOwner(state, userId);
     }
 
