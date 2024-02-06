01a824f830019f3c0f4343e8a67c2879d635e3df
wcobq-home <tolmachev.e.s@yandex.ru>
2024-02-07 02:04:51 +0300

    удалены настройки

--- src/test/java/ru/practicum/shareit/booking/controllers/BookingControllerTest.java
+++ src/test/java/ru/practicum/shareit/booking/controllers/BookingControllerTest.java
@@ -10,12 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -31,7 +25,6 @@ import java.time.LocalTime;
 import java.util.Set;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -48,7 +41,7 @@ class BookingControllerTest {
 
     @BeforeEach
     void init() {
-         mockMvc = MockMvcBuilders
+        mockMvc = MockMvcBuilders
                 .standaloneSetup(controller)
                 .build();
 
